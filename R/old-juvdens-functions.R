bf1d616dd8211b49ebe1084b52f04fcb7ef91ee9
colinpmillar <colinpmillar@googlemail.com>
2015-01-17 22:25:39 +0000

    adjusted tolerance on simQ

--- R/old-juvdens-functions.R
+++ R/old-juvdens-functions.R
@@ -59,9 +59,9 @@ simplify.graph <- function(g) {
 #' Q <- getQMat(wk_nb)
 #' x <- simQ(exp(-4)*Q)
 #' plot(wk, col = grey( pmax(0, pmin(1, (x + 5) / 10))))
-simQ <- function(Q) {
+simQ <- function(Q, tol = 1e-9) {
   eQ <- eigen(Q)
-  rank <- sum(eQ $ values > 1e-12)
+  rank <- sum(eQ $ values > tol)
   colSums(t(eQ $ vectors[,1:rank]) * rnorm(rank, 0, 1/sqrt(eQ $ values[1:rank])))
 }
 
