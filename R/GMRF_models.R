b99f26dda366166d0c84d4d4b6f766b7da2d5bfc
colinpmillar <colinpmillar@googlemail.com>
2015-01-17 17:04:13 +0000

    GMRF extensions to mgcv

---
+++ R/GMRF_models.R
@@ -0,0 +1,70 @@
+smooth.construct.gmrf.smooth.spec <- function(object, data, knots) {
+    x <- data[[object$term]]
+    x <- factor(x, levels = sort(x))
+    k <- knots[[object$term]]
+    if (is.null(k)) k <- factor(levels(x), levels = levels(x))
+    else k <- as.factor(k)
+    if (object$bs.dim < 0) 
+        object$bs.dim <- length(levels(k))
+    if (object$bs.dim > length(levels(k))) 
+        stop("GMRF basis dimension set too high")
+    if (sum(!levels(x) %in% levels(k))) 
+        stop("data contain regions that are not contained in the knot specification")
+    x <- factor(x, levels = levels(k))
+    object$X <- model.matrix(~x - 1, )
+    # penalty
+    object$S[[1]] <- object$xt$penalty
+    if (ncol(object$S[[1]]) != nrow(object$S[[1]])) 
+        stop("supplied penalty not square!")
+    if (ncol(object$S[[1]]) != ncol(object$X)) 
+        stop("supplied penalty wrong dimension!")
+    if (!is.null(colnames(object$S[[1]]))) {
+        a.name <- colnames(object$S[[1]])
+        if (all.equal(levels(k), sort(a.name)) != TRUE) {
+            stop("penalty column names don't match supplied area names!")
+        }
+        else {
+            if (all.equal(sort(a.name), a.name) != TRUE) {
+              object$S[[1]] <- object$S[[1]][levels(k), ]
+              object$S[[1]] <- object$S[[1]][, levels(k)]
+            }
+        }
+    }
+
+    if (object$bs.dim < length(levels(k))) {
+        mi <- which(colSums(object$X) == 0)
+        np <- ncol(object$X)
+        if (length(mi) > 0) {
+            object$X <- rbind(matrix(0, length(mi), np), object$X)
+            for (i in 1:length(mi)) object$X[i, mi[i]] <- 1
+        }
+        rp <- mgcv:::nat.param(object$X, object$S[[1]], type = 0)
+        ind <- (np - object$bs.dim + 1):np
+        object$X <- if (length(mi)) 
+            rp$X[-(1:length(mi)), ind]
+        else rp$X[, ind]
+        object$P <- rp$P[, ind]
+        object$S[[1]] <- diag(c(rp$D[ind[ind <= rp$rank]], rep(0, 
+            sum(ind > rp$rank))))
+        object$rank <- rp$rank
+    }
+    else {
+        ev <- eigen(object$S[[1]], symmetric = TRUE, only.values = TRUE)$values
+        object$rank <- sum(ev > .Machine$double.eps^0.8 * max(ev))
+    }
+    object$null.space.dim <- ncol(object$X) - object$rank
+    object$knots <- k
+    object$df <- ncol(object$X)
+    class(object) <- "gmrf.smooth"
+    object
+}
+ 
+
+Predict.matrix.gmrf.smooth <- function (object, data) {
+    x <- factor(data[[object$term]], levels = levels(object$knots))
+    X <- model.matrix(~x - 1)
+    if (!is.null(object$P)) 
+        X <- X %*% object$P
+    X
+}
+
